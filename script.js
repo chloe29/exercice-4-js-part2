ef483952e72f2b4c0b67892f37677c780db117a6
lavisse <chloe.lavisse80@gmail.com>
2018-10-10 10:45:43 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,6 @@
+function effacer(){
+  document.getElementById('lastName').value='';
+  document.getElementById('firstName').value='';
+  document.getElementById('city').value='';
+}
+// on peut remplacer '' par null
